8937b447755e1eaa1b552f084e4fde64fa1ad74a
Scott Harris <scottsharris@yahoo.com>
2020-06-24 15:46:10 -0700

    added init and action functions

--- seeds.sql
+++ seeds.sql
@@ -1,17 +1,19 @@
+USE employees_db;
+
 -- Inserting Departments
-INSERT INTO department (name)
+INSERT INTO department (department)
 VALUES ("Sales");
-INSERT INTO department (name)
+INSERT INTO department (department)
 VALUES ("Accounting");
-INSERT INTO department (name)
+INSERT INTO department (department)
 VALUES ("Warehouse");
-INSERT INTO department (name)
+INSERT INTO department (department)
 VALUES ("Customer Service");
-INSERT INTO department (name)
+INSERT INTO department (department)
 VALUES ("Reception");
-INSERT INTO department (name)
+INSERT INTO department (department)
 VALUES ("Quality Assurance");
-INSERT INTO department (name)
+INSERT INTO department (department)
 VALUES ("Human Resources");
 
 -- Inserting Roles
@@ -37,3 +39,33 @@ INSERT INTO role (title, salary, department_id)
 VALUES ("Human Resources Specialist", 65120, 7);
 
 -- Inserting Employees
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Michael", "Scott", 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Dwight", "Schrute", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jim", "Halpert", 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Pam", "Beesly", 8, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Andy", "Bernard", 3, 1);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Darryl", "Philbin", 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Nate", "Nickerson", 6, 6);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Lonny", "Collins", 6, 6);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Angela", "Martin", 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Oscar", "Martinez", 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kevin", "Malone", 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kelly", "Kapoor", 7, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Creed", "Bratton", 9, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Meredith", "Palmer", 9, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Toby", "Flenderson", 10, 1);
---
+++ start.js
@@ -0,0 +1,129 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+let departments = [];
+let roles = [];
+let managers = ["None"];
+
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "employees_db"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    init();
+})
+
+function init() {
+    console.log(
+        `
+       ------------------------------------------------------------------------------
+       | 8888888888                        888                                      |
+       | 888                               888                                      |
+       | 888                               888                                      |
+       | 8888888    88888b.d88b.  88888b.  888  .d88b.  888  888  .d88b.   .d88b.   |
+       | 888        888 "888 "88b 888 "88b 888 d88""88b 888  888 d8P  Y8b d8P  Y8b  |
+       | 888        888  888  888 888  888 888 888  888 888  888 88888888 88888888  |
+       | 888        888  888  888 888 d88P 888 Y88..88P Y88b 888 Y8b.     Y8b.      |
+       | 8888888888 888  888  888 88888P"  888  "Y88P"   "Y88888  "Y8888   "Y8888   |
+       |                          888                        888                    |
+       |                          888                   Y8b d88P                    |
+       |                          888                    "Y88P"                     |
+       | 888b     d888                                                              |
+       | 8888b   d8888                                                              |
+       | 88888b.d88888                                                              |
+       | 888Y88888P888  8888b.  88888b.   8888b.   .d88b.   .d88b.  888d888         |
+       | 888 Y888P 888     "88b 888 "88b     "88b d88P"88b d8P  Y8b 888P"           |
+       | 888  Y8P  888 .d888888 888  888 .d888888 888  888 88888888 888             |
+       | 888   "   888 888  888 888  888 888  888 Y88b 888 Y8b.     888             |
+       | 888       888 "Y888888 888  888 "Y888888  "Y88888  "Y8888  888             |
+       |                                               888                          |
+       |                                          Y8b d88P                          |
+       |                                           "Y88P"                           |
+       ------------------------------------------------------------------------------                                                                 
+        `
+    );
+    action();
+};
+
+function action() {
+    getDepts();
+    getRoles();
+    getMgrs();
+    inquirer.prompt({
+        type: "list",
+        name: "action",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "View All Employees By Department",
+            "View All Employees By Manager",
+            "View All Employees By Role",
+            "Add New Employee",
+            "Add New Role",
+            "Add New Department",
+            "Update Employee Role",
+            "Update Employee Manager",
+            "Delete Department",
+            "Delete Role"
+        ]
+    }).then((answer) => {
+        switch (answer.action) {
+            case "View All Employees":
+                viewAll();
+                break;
+
+            case "View All Employees By Department":
+                viewAllByDepartment();
+                break;
+
+            case "View All Employees By Manager":
+                viewAllByManager();
+                break;
+
+            case "View All Employees By Role":
+                viewAllByRole();
+                break;
+
+            case "Add New Employee":
+                addEmployee();
+                break;
+
+            case "Add New Role":
+                addRole();
+                break;
+
+            case "Add New Department":
+                addDept();
+                break;
+
+            case "Update Employee Role":
+                updateRole();
+                break;
+
+            case "Update Employee Manager":
+                updateMgr();
+                break;
+
+            case "Delete Department":
+                deleteDept();
+                break;
+
+            case "Delete Role":
+                deleteRole();
+                break;
+
+        }
+    });
+};
+
